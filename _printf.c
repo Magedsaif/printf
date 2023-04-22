2b4d9b35f839b13e65bb4b7f053ec892e6f63a56
Magedsaif <magedsaif21@gmail.com>
2023-04-22 17:29:14 +0200

    task 0 with betty

--- _printf.c
+++ _printf.c
@@ -1,104 +1,111 @@
-/*
- * 0x06. C - More pointers, arrays and strings
- */
 #include "main.h"
-#include <string.h> 
+#include <string.h>
 #include <unistd.h>
+/**
+*write_string - a function that writes a string to the standard output
+ *@args:variadic arguments
+ *Return:the number of characters printed
+*/
 int write_string(va_list args)
 {
 	int len;
 	char *str;
 
 	str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
 	len = strlen(str);
-	write (STDOUT_FILENO, str, len);
+	write(STDOUT_FILENO, str, len);
 
 	return (len);
-
 }
+/**
+ *write_char - a function that writes a char to the standard output
+ *@args:variadic arguments
+ *Return:the number of characters printed
+*/
 int write_char(va_list args)
 {
 	char c;
 
 	c = (char) va_arg(args, int);
-	write (STDOUT_FILENO, &c,1);
+	write(STDOUT_FILENO, &c, 1);
 
-	return 1;
+	return (1);
 }
-
+/**
+ *write_specifier - a function that writes a specifier to the standard output
+ *@args:variadic arguments
+ *Return:the number of characters printed
+*/
 int write_specifier(va_list args)
 {
 	(void) args;
-	write (STDOUT_FILENO, "%",1);
+	write(STDOUT_FILENO, "%", 1);
 
-	return 1;
+	return (1);
 }
-
-
+/**
+*is_specifier - a function that checks if there is a specifier in the format
+ *@spacifiers: array of structs containing different conversion specifiers
+ *@format: pointer to an array containing the string of chars to be handled
+ *Return: -1 in case of reaching the end of format
+ *		  (i) in case of finding a match to the specifier
+*/
 int is_specifier(specifier_t spacifiers[], char *format)
 {
 	int i;
 	char fchar;
+
 	i = 0;
-	if(format[i] == '%')
+	if (format[i] == '%')
 	{
-		if(format[i + 1] == '\0')
+		if (format[i + 1] == '\0')
 			return (-1);/*error*/
-		else
+
+		fchar = format[i + 1];
+		/*select spacifier function*/
+		for (i = 0; i < 3; i++)
 		{
-			fchar = format[i + 1];
-			/*select spacifier function*/
-			for (i = 0; i < 3; i++)
-			{
-				
-				if(spacifiers[i].s == fchar)
-					return (i);
-			}
+			if (spacifiers[i].s == fchar)
+				return (i);
 		}
-		
 	}
-		
 	/*not found*/
 	return (-2);
 }
 /**
- * _strcmp - copies a string.
- * @s1: string refrance
- * @s2: string refrance
- * Return: 0 in equity
+ *_printf - a function to print a string using a format
+ *@format: pointer to an array containing the string of chars to be handled
+ *Return: count or error upon failing (-1)
  */
 int _printf(const char *format, ...)
 {
 	int i, s_index, write_count;
 	va_list args;
-	specifier_t spacifiers[] = {{'s', write_string}, {'c', write_char}, {'%', write_specifier}};
-	
+	specifier_t spacifiers[] = {
+	{'s', write_string}, {'c', write_char}, {'%', write_specifier}};
+
+	if (format == NULL)
+		return (-1);
+
 	va_start(args, format);
 	write_count = 0;
-	for (i = 0;(format != NULL) && (format[i] != '\0'); i++)
+	for (i = 0; (format != NULL) && (format[i] != '\0'); i++)
 	{
 		s_index = is_specifier(spacifiers, (char *) (format + i));
-		
-		if(s_index == -2)
-			/*print char*/
-			{
-				write (STDOUT_FILENO, (char *) (format + i), 1);
-				
-				write_count ++;
-			}
-		else if(s_index == -1)
-			/*print char*/
+
+		if (s_index == -2)
+		{
+			write(STDOUT_FILENO, (char *) (format + i), 1);
+			write_count++;
+		}
+		else if (s_index == -1)
 			return (-1);
-		else
 		{
 			write_count = write_count + spacifiers[s_index].f(args);
 			i++;
 		}
-			
-		
-
 	}
-	
-	return (write_count);
-
+return (write_count);
 }
--- test/test.c
+++ test/test.c
@@ -11,6 +11,11 @@ int main(void)
 {
 	int len, len2;
 
+	len = _printf("");
+    len2 = printf("");
+	printf("len %d\n", len);
+	printf("len2 %d\n", len2);
+	
 	len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
 	printf("len %d\n", len);
@@ -22,6 +27,23 @@ int main(void)
 	printf("len2 %d\n", len2);
 	len = _printf("%%\n");
 	len2 = printf("%%\n");
+	printf("len %d\n", len);
+	printf("len2 %d\n", len2);
+
+	
+	len = _printf(NULL);
+	len2 = printf(NULL);
+	printf("len %d\n", len);
+	printf("len2 %d\n", len2);
+
+	len = _printf(NULL, NULL);
+	len2 = printf(NULL, NULL);
+	printf("len %d\n", len);
+	printf("len2 %d\n", len2);
+
+
+	len = _printf("%s %c %%\n", NULL);
+	len2 = printf("%s %c %%\n", NULL);
 	printf("len %d\n", len);
 	printf("len2 %d\n", len2);
 	return 0;
