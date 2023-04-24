4682cf8af47a6b3fc74803c2457515d03f5a0c15
Magedsaif <magedsaif21@gmail.com>
2023-04-24 15:43:48 +0200

    task 3 octal

---
+++ task_3(print o).c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <limits.h>
+
+void helpers(int ocount, unsigned int num)
+{	
+	int reminder;
+	char octal;
+
+	if (ocount == 0)
+		return;
+
+	reminder = num % 8 ;
+	octal = reminder + '0';
+	num = num / 8;
+	helpers((ocount - 1), num);
+	write(STDOUT_FILENO, &octal, 1);
+}
+/**
+ *write_signed_number - a function that writes signed decimal integer
+ *@args:variadic arguments
+ *Return:the number of characters printed
+*/
+int write_octal_number(va_list args)
+{
+	unsigned int num;
+	unsigned int temp,ocount;
+	char octal;
+
+	num = va_arg(args, int);
+	if (num == 1 || num == 0)
+	{
+		octal = '0' + num;
+		write(STDOUT_FILENO, &octal, 1);
+		return (1);
+	}
+
+	temp = num;
+	ocount = 0;
+	while (temp != 0)
+	{
+		temp = temp / 8;
+		ocount++;
+	}
+	helpers((ocount - 1), num);
+	return (ocount);
+}
